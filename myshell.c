edc3de13c221e78c24fb4501c54e4a50d626c9af
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-06 15:10:42 +0200

    Create myshell.c

---
+++ myshell.c
@@ -0,0 +1,13 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int prepare(void);
+int process_arglist(int count, char **arglist);
+int finalize(void);
+
+int main(void)
+{
+}
