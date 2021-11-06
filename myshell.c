416f8a854ab5c94b5b76f6d3e9f5c63b1531d67d
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-06 18:50:44 +0200

    first draft

--- myshell.c
+++ myshell.c
@@ -10,8 +10,17 @@ int prepare(void)
 }
 
 int process_arglist(int count, char **arglist) {
+	int flag, son_exit_status;
+
 	if (strcmp(arglist[count], "&") == 0) { // process needs to run in the background
-		// TODO
+		arglist[count] = NULL; // not send "&" symbol to son
+		int pid = fork();
+		if (pid == 0) { // son executes the command
+			execvp(arglist[0], arglist);
+		}
+		else { // parent doesn't wait for son to finish
+			
+		}
 	}
 	if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
 		// TODO
@@ -19,13 +28,19 @@ int process_arglist(int count, char **arglist) {
 	else {
 		for (int i = 0; i < count; i++) {
 			if (strcmp(arglist[i], "|") == 0) { // command has a pipe
-			// TODO
-			flag = 1;
-			break;
+				// TODO
+				flag = 1;
+				break;
 			}
 		}
 		if (flag == 0) { // regular single command
-			// TODO
+			int pid = fork();
+			if (pid == 0) { // son executes the command
+				execvp(arglist[0], arglist);
+			}
+			else { // parent waits for son to finish
+				waitpid(pid, &son_exit_status, 0);
+			}
 		}	
 	}
 }
