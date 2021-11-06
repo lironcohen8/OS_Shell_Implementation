dab5095b4933288f415a198edc075d055624bc57
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-06 22:26:32 +0200

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -18,16 +18,16 @@ int process_arglist(int count, char **arglist) {
 			arglist[count] = NULL; // not send "&" symbol to execvp
 			execvp(arglist[0], arglist);
 		}
-		// parent doesn't wait for son to finish
+		// parent does not wait for son to finish
 	}
 
-	if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
+	else if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
 		char *filename = arglist[count];
 		int fd = open(filename, O_CREAT|O_WRONLY);
 		int pid = fork();
 		if (pid == 0) { // son executes th command
 			arglist[count-1] = NULL; // not send ">" symbol and filename to execvp
-			dup2(fd, 1); // make standard output of son to be fd
+			dup2(fd, stdout); // make standard output of son to be fd
 			execvp(arglist[0], arglist);
 		}
 		else { // parent waits for son to finish
@@ -44,9 +44,8 @@ int process_arglist(int count, char **arglist) {
 				if (pid1 == 0) { // first son executes the first command
 					close(pipefds[0]); // closing reading pd for writing son
 					arglist[i] = NULL; // first son only gets first command
-					dup2(pipefds[1], 1); // make standard output of first son to be fd1
+					dup2(pipefds[1], stdout); // make standard output of first son to be fd1
 					execvp(arglist[0], arglist);
-					// TODO close fd
 				}
 				else { // parent
 					waitpid(pid1, &son_exit_status, 0); // waits for first son to finish
@@ -54,12 +53,13 @@ int process_arglist(int count, char **arglist) {
 					if (pid2 == 0) { // second son executes the second command
 						close(pipefds[1]); // closing writing pd for reading son
 						arglist[i] = arglist[0]; // copy name to the first argument of the second command
-						dup2(pipefds[0], 0); // make standard input of second son to be fd0
+						dup2(pipefds[0], stdin); // make standard input of second son to be fd0
 						execvp(arglist[i], arglist[i]); // only sending the second command
-						// TODO close fd
 					}
 					else {
 						waitpid(pid2, &son_exit_status, 0); // parent waits for second son to finish
+						close(pipefds[0]);
+						close(pipefds[1]);
 					}
 				}
 				flag = 1;
