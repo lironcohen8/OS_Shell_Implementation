9df1ceb0575e8348a60f90d0dec1dda7916bb9a4
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-06 19:37:03 +0200

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -20,6 +20,7 @@ int process_arglist(int count, char **arglist) {
 		}
 		// parent doesn't wait for son to finish
 	}
+
 	if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
 		char *filename = arglist[count];
 		int fd = open(filename, O_CREAT|O_WRONLY);
@@ -33,14 +34,40 @@ int process_arglist(int count, char **arglist) {
 			waitpid(pid, &son_exit_status, 0);
 		}
 	}
+
 	else {
 		for (int i = 0; i < count; i++) {
 			if (strcmp(arglist[i], "|") == 0) { // command has a pipe
-				// TODO
+				int pipefds[2]; // array for fds from pipe 
+				pipe(pipefds);
+				int pid1 = fork();
+				if (pid1 == 0) { // first son executes the first command
+					close(pipefds[0]); // closing reading pd for writing son
+					arglist[i] = NULL; // first son only gets first command
+					dup2(pipefds[1], 1); // make standard output of first son to be fd1
+					execvp(arglist[0], arglist);
+					// TODO close fd
+				}
+				else { // parent
+					waitpid(pid1, &son_exit_status, 0); // waits for first son to finish
+					int pid2 = fork();
+					if (pid2 == 0) { // second son executes the second command
+						close(pipefds[1]); // closing writing pd for reading son
+						arglist[i] = arglist[0]; // copy name to the first argument of the second command
+						dup2(pipefds[0], 0); // make standard input of second son to be fd0
+						execvp(arglist[i], arglist[i]); // only sending the second command
+						// TODO close fd
+					}
+					else {
+						waitpid(pid2, &son_exit_status, 0); // parent waits for second son to finish
+					}
+				}
 				flag = 1;
 				break;
 			}
 		}
+		// TODO add errors where needed
+
 		if (flag == 0) { // regular single command
 			int pid = fork();
 			if (pid == 0) { // son executes the command
