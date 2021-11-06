67c30d347b9d795e048317a643f6b8a82a0f0d91
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-06 18:15:04 +0200

    added cases recongizion

--- myshell.c
+++ myshell.c
@@ -4,9 +4,36 @@
 #include <string.h>
 #include <errno.h>
 
-int prepare(void);
-int process_arglist(int count, char **arglist);
-int finalize(void);
+int prepare(void)
+{
+	return 0;
+}
+
+int process_arglist(int count, char **arglist) {
+	if (strcmp(arglist[count], "&") == 0) { // process needs to run in the background
+		// TODO
+	}
+	if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
+		// TODO
+	}
+	else {
+		for (int i = 0; i < count; i++) {
+			if (strcmp(arglist[i], "|") == 0) { // command has a pipe
+			// TODO
+			flag = 1;
+			break;
+			}
+		}
+		if (flag == 0) { // regular single command
+			// TODO
+		}	
+	}
+}
+
+int finalize(void)
+{
+	return 0;
+}
 
 int main(void)
 {
