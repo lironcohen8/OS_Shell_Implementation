561fa3ea1ccce9ef1e4626b306c0a466e340ceed
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-07 21:54:43 +0200

    added sigints

--- myshell.c
+++ myshell.c
@@ -3,43 +3,47 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 int prepare(void)
 {
+	signal(SIGINT, SIG_IGN); // shell should not terminate upon SIGINT
 	return 0;
 }
 
 int process_arglist(int count, char **arglist) {
-	int flag, son_exit_status;
+	int flag, child_exit_status;
 
 	if (strcmp(arglist[count], "&") == 0) { // process needs to run in the background
 		int pid = fork();
-		if (pid == 0) { // son executes the command
+		if (pid == 0) { // child executes the command
 			arglist[count] = NULL; // not send "&" symbol to execvp
 			execvp(arglist[0], arglist);
 		}
-		// parent does not wait for son to finish
+		// parent does not wait for child to finish
 	}
 
 	else if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
+		signal(SIGINT, SIG_DFL); // foreground child should terminate upon SIGINT
 		char *filename = arglist[count];
 		int fd = open(filename, O_WRONLY | O_CREAT, 00777);
 		int pid = fork();
-		if (pid == 0) { // son executes th command
+		if (pid == 0) { // child executes th command
 			arglist[count-1] = NULL; // not send ">" symbol and filename to execvp
-			dup2(fd, 1); // make standard output of son to be fd
+			dup2(fd, 1); // make standard output of child to be fd
 			execvp(arglist[0], arglist);
 		}
-		else { // parent waits for son to finish
-			waitpid(pid, &son_exit_status, 0);
+		else { // parent waits for child to finish
+			waitpid(pid, &child_exit_status, 0);
 		}
 	}
 
 	else {
 		for (int i = 0; i < count; i++) {
 			if (strcmp(arglist[i], "|") == 0) { // command has a pipe
+				signal(SIGINT, SIG_DFL); // foreground child should terminate upon SIGINT
 				int pipefds[2]; // array for fds from pipe 
 				int pipe_status = pipe(pipefds);
 				if (pipe_status == -1) {
@@ -47,23 +51,23 @@ int process_arglist(int count, char **arglist) {
     				return 0;
 				}
 				int pid1 = fork();
-				if (pid1 == 0) { // first son executes the first command
-					close(pipefds[0]); // closing reading pd for writing son
-					arglist[i] = NULL; // first son only gets first command
-					dup2(pipefds[1], 1); // make standard output of first son to be fd1
+				if (pid1 == 0) { // first child executes the first command
+					close(pipefds[0]); // closing reading pd for writing child
+					arglist[i] = NULL; // first child only gets first command
+					dup2(pipefds[1], 1); // make standard output of first child to be fd1
 					execvp(arglist[0], arglist);
 				}
 				else { // parent
-					waitpid(pid1, &son_exit_status, 0); // waits for first son to finish
+					waitpid(pid1, &child_exit_status, 0); // waits for first child to finish
 					int pid2 = fork();
-					if (pid2 == 0) { // second son executes the second command
-						close(pipefds[1]); // closing writing pd for reading son
+					if (pid2 == 0) { // second child executes the second command
+						close(pipefds[1]); // closing writing pd for reading child
 						arglist[i] = arglist[0]; // copy name to the first argument of the second command
-						dup2(pipefds[0], 0); // make standard input of second son to be fd0
+						dup2(pipefds[0], 0); // make standard input of second child to be fd0
 						execvp(arglist[i], &arglist[i]); // only sending the second command
 					}
 					else {
-						waitpid(pid2, &son_exit_status, 0); // parent waits for second son to finish
+						waitpid(pid2, &child_exit_status, 0); // parent waits for second child to finish
 						close(pipefds[0]);
 						close(pipefds[1]);
 					}
@@ -75,12 +79,13 @@ int process_arglist(int count, char **arglist) {
 		// TODO add errors where needed
 
 		if (flag == 0) { // regular single command
+			signal(SIGINT, SIG_DFL); // foreground child should terminate upon SIGINT
 			int pid = fork();
-			if (pid == 0) { // son executes the command
+			if (pid == 0) { // child executes the command
 				execvp(arglist[0], arglist);
 			}
-			else { // parent waits for son to finish
-				waitpid(pid, &son_exit_status, 0);
+			else { // parent waits for child to finish
+				waitpid(pid, &child_exit_status, 0);
 			}
 		}	
 	}
