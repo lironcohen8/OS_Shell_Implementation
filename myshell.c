d0fcceb043c481b000076575df328e1384639c18
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-07 21:05:45 +0200

    fix for compilation

--- myshell.c
+++ myshell.c
@@ -1,8 +1,10 @@
-#define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int prepare(void)
 {
@@ -23,11 +25,11 @@ int process_arglist(int count, char **arglist) {
 
 	else if (strcmp(arglist[count-1], ">") == 0) { // command has output redirecting
 		char *filename = arglist[count];
-		int fd = open(filename, O_CREAT|O_WRONLY);
+		int fd = open(filename, O_WRONLY | O_CREAT, 00777);
 		int pid = fork();
 		if (pid == 0) { // son executes th command
 			arglist[count-1] = NULL; // not send ">" symbol and filename to execvp
-			dup2(fd, stdout); // make standard output of son to be fd
+			dup2(fd, 1); // make standard output of son to be fd
 			execvp(arglist[0], arglist);
 		}
 		else { // parent waits for son to finish
@@ -39,12 +41,16 @@ int process_arglist(int count, char **arglist) {
 		for (int i = 0; i < count; i++) {
 			if (strcmp(arglist[i], "|") == 0) { // command has a pipe
 				int pipefds[2]; // array for fds from pipe 
-				pipe(pipefds);
+				int pipe_status = pipe(pipefds);
+				if (pipe_status == -1) {
+					printf("Error creating pipe: %s\n", strerror(errno));
+    				return 0;
+				}
 				int pid1 = fork();
 				if (pid1 == 0) { // first son executes the first command
 					close(pipefds[0]); // closing reading pd for writing son
 					arglist[i] = NULL; // first son only gets first command
-					dup2(pipefds[1], stdout); // make standard output of first son to be fd1
+					dup2(pipefds[1], 1); // make standard output of first son to be fd1
 					execvp(arglist[0], arglist);
 				}
 				else { // parent
@@ -53,8 +59,8 @@ int process_arglist(int count, char **arglist) {
 					if (pid2 == 0) { // second son executes the second command
 						close(pipefds[1]); // closing writing pd for reading son
 						arglist[i] = arglist[0]; // copy name to the first argument of the second command
-						dup2(pipefds[0], stdin); // make standard input of second son to be fd0
-						execvp(arglist[i], arglist[i]); // only sending the second command
+						dup2(pipefds[0], 0); // make standard input of second son to be fd0
+						execvp(arglist[i], &arglist[i]); // only sending the second command
 					}
 					else {
 						waitpid(pid2, &son_exit_status, 0); // parent waits for second son to finish
@@ -78,6 +84,7 @@ int process_arglist(int count, char **arglist) {
 			}
 		}	
 	}
+	return 1;
 }
 
 int finalize(void)
